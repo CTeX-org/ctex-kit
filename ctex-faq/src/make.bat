e4c2b703570d412848da5dc706239242fd47d4e3
yuleopen <yuleopen@3a008252-3892-11de-8a2e-13aa1706fec1>
2009-05-24 03:38:31 +0000

    CTeX-FAQ by aloft. This is version 0.3 from ctex.org

---
+++ ctex-faq/src/make.bat
@@ -0,0 +1,21 @@
+@echo off
+latex ctex-faq.tex
+makeindex -o ctex-faq.ind ctex-faq.idx
+latex ctex-faq.tex
+makeindex -o ctex-faq.ind ctex-faq.idx
+latex ctex-faq.tex
+gbk2uni ctex-faq.dvi
+latex ctex-faq.tex
+dvipdfmx ctex-faq.dvi
+del *.aux
+del *.toc
+del *.lon
+del *.lor
+del *.ilg
+del *.idx
+del *.ind
+del *.out
+del *.log
+del *.exa
+@echo on
+pause
