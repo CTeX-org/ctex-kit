7291a6ed5005a7938e4698b4c5542b075dc94ec6
sobenlee <sobenlee@3a008252-3892-11de-8a2e-13aa1706fec1>
2012-04-21 13:22:50 +0000

    ctex 通过应用 luatexja 宏包支持 LuaLaTeX。

---
+++ trunk/ctex/engine/jfm-plain.lua
@@ -0,0 +1,9 @@
+luatexja.jfont.define_jfm {
+   dir = 'yoko',
+   zw = 1.0, zh = 1.0,
+
+   [0] = {
+      align = 'left', left = 0.0, down = 0.0,
+      width = 1.0, height = 0.88, depth = 0.12, italic=0.0,
+   }
+}
