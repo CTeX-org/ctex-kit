b82c6991bfd8fea1c3489ab001fe7a5e92335d1a
Qing Lee <sobenlee@gmail.com>
2015-05-03 07:21:10 +0800

    tool: 增加 zhconv.lua 用于 UTF-8 到 GBK/Big5 的编码转换。

--- tool/zhl3build.lua
+++ tool/zhl3build.lua
@@ -18,6 +18,7 @@
 
 gitverfiles = gitverfiles or unpackfiles
 gbkfiles = gbkfiles or { }
+big5files = big5files or { }
 generic_insatllfiles = generic_insatllfiles or { }
 subtexdirs = subtexdirs or { }
 
@@ -29,6 +30,8 @@ local function script_path()
 end
 
 dtxchecksum = dofile(script_path() .. "dtxchecksum.lua").checksum
+zhconv = dofile(script_path() .. "zhconv.lua").conv
+
 -- 只对 .dtx 进行 \CheckSum 校正
 function checksum()
   unpack ()
@@ -88,13 +91,18 @@ function hooked_bundleunpack()
   -- UTF-8 to GBK conversion
   for _,f in ipairs(gbkfiles) do
     local f_utf = unpackdir .. "/" .. f
-    local f_gbk = unpackdir .. "/" .. f .. ".gbk"
     if os_windows then
       f_utf = unix_to_win(f_utf)
-      f_gbk = unix_to_win(f_gbk)
     end
-    os.execute("iconv -f utf-8 -t gbk " .. f_utf .. " > " .. f_gbk)
-    mv(f_gbk, f_utf)
+    zhconv(f_utf, f_utf)
+  end
+  -- UTF-8 to Big5 conversion
+  for _,f in ipairs(big5files) do
+    local f_utf = unpackdir .. "/" .. f
+    if os_windows then
+      f_utf = unix_to_win(f_utf)
+    end
+    zhconv(f_utf, f_utf, "big5")
   end
 end
 
