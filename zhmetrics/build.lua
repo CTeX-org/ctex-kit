ab58dbe8b179f32e64553525bdfb88170afa0d7b
Qing Lee <sobenlee@gmail.com>
2015-05-20 08:48:15 +0800

    zhmCJK: 增加编译脚本 build.lua。

---
+++ zhmetrics/build.lua
@@ -0,0 +1,59 @@
+#!/usr/bin/env texlua
+
+module = "zhmcjk"
+
+packtdszip = true
+
+gitverfiles = { }
+sourcefiles = {"zhmCJK.dtx", "zhmCJK.ins", "zhmCJK.lua", "zhmCJK-addmap.pl"}
+unpackfiles = {"zhmCJK.ins"}
+installfiles = {"zhmCJK.sty"}
+typesetfiles = {"zhmCJK.dtx", "zhmCJK-test.tex"}
+binaryfiles = {"*.pdf", "*.tfm", "*.tar.bz2"}
+unpackexe = "pdftex"
+typesetexe = "latex"
+makeindexexe = "zhmakeindex"
+
+dtxchecksum = dtxchecksum or { }
+dtxchecksum.exe = "latex"
+
+function doc_prehook()
+  cp("zhmCJK-test.tex", unpackdir, ".")
+end
+
+function doc_posthook()
+  for _, i in pairs(typesetfiles) do
+    local name = stripext(i)
+    run(typesetdir, "dvipdfmx " .. name)
+    cp(name .. ".pdf", typesetdir, ".")
+  end
+  os.remove("zhmCJK-test.tex")
+end
+
+function copytds_posthook()
+  cp("zhmCJK-test.tex", typesetdir, tdsdir .. "/doc/" .. moduledir)
+  mkdir(supportdir)
+  cp("zhmCJK.lua", ".", supportdir)
+  run(supportdir, "texlua zhmCJK.lua map")
+  if not lfs.isfile(supportdir .. "/fallback.tar.bz2") then
+    run(supportdir, "texlua zhmCJK.lua nomap")
+    run(supportdir, "tar --remove-files -cjf fallback.tar.bz2 fallback")
+  end
+  local ctandir = ctandir .. "/" .. ctanpkg
+  local mapdir = tdsdir .. "/fonts/map/fontname"
+  mkdir(mapdir)
+  for _, i in pairs{"texfonts.map", "zhmCJK.map"} do
+    cp(i, supportdir, ctandir)
+    cp(i, supportdir, mapdir)
+  end
+  local tfmdir = tdsdir .. "/fonts/tfm/" .. ctanpkg
+  mkdir(tfmdir)
+  for _, i in pairs{"zhmCJK.tfm", "fallback.tar.bz2"} do
+    cp(i, supportdir, ctandir)
+    cp(i, supportdir, tfmdir)
+  end
+end
+
+dofile("../tool/zhl3build.lua")
+
+-- vim:sw=2:et
