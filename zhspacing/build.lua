82fd40c02d11951f3f1fd0c5bca633d1546576ea
Qing Lee <sobenlee@gmail.com>
2016-02-10 16:43:27 +0800

    zhspacing: 使用 l3build 来构建。

---
+++ zhspacing/build.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env texlua
+
+bundle = ""
+module = "zhspacing"
+
+packtdszip  = true
+tdsroot     = "generic"
+
+unpackfiles  = { }
+installfiles = {"*.sty", "t-zhspacing.tex"}
+sourcefiles  = installfiles
+typesetfiles = {"zhs-man.tex"}
+demofiles    = {"zhspacing-context-test.tex", "README"}
+typesetexe   = "xelatex"
+
+context_insatllfiles = {"t-zhspacing.tex"}
+xelatex_insatllfiles = {"zhfont.sty", "zhulem.sty"}
+
+dofile("../tool/zhl3build.lua")
