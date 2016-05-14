25c05913234cd5e3d2738b81e98865956c71215f
Qing Lee <sobenlee@gmail.com>
2016-05-14 12:22:35 +0800

    xpinyin: 更新文档格式，应用 ctxdoc 和 ctxdocstrip，README 改成 md

--- xpinyin/build.lua
+++ xpinyin/build.lua
@@ -7,16 +7,18 @@ packtdszip = true
 gitverfiles = {"xpinyin.dtx"}
 sourcefiles = {"xpinyin.dtx", "xpinyin.ins"}
 unpackfiles = {"xpinyin.ins"}
-unpacksuppfiles = {"xpinyin.ver", "xpinyin.db"}
+unpacksuppfiles = {"xpinyin.id", "xpinyin.db", "ctxdocstrip.tex"}
+typesetsuppfiles = {"ctxdoc.cls"}
 installfiles = {"*.sty", "*.def"}
 unpackexe = "luatex"
 typesetexe = "xelatex"
 
 function unpack_prehook()
   cleandir(unpackdir)
+  cp("ctxdocstrip.tex", supportdir, ".")
   os.execute(unpackexe .. " -output-directory=" .. unpackdir .." xpinyin.dtx > " .. os_null)
+  os.remove("ctxdocstrip.tex")
   cp("xpinyin.ins", unpackdir, ".")
-  mkdir(supportdir)
   cp("xpinyin.lua", unpackdir, supportdir)
   run(supportdir, "texlua xpinyin.lua")
 end
@@ -26,8 +28,8 @@ function unpack_posthook()
 end
 
 function copytds_posthook()
-  cp("xpinyin.ins", unpackdir, tdsdir .. "/source/" .. moduledir)
   cp("xpinyin.ins", unpackdir, ctandir .. "/" .. ctanpkg)
+  cp("xpinyin.ins", unpackdir, tdsdir .. "/source/" .. moduledir)
 end
 
 dofile("../tool/zhl3build.lua")
