389280c7eafa902afe88904ee79c3951c0192ab2
Qing Lee <sobenlee@gmail.com>
2016-05-18 20:32:22 +0800

    统一的发布脚本 ctan.lua

---
+++ ctan.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env texlua
+
+local ctex_kit = {
+  "CJKpunct"        ,
+  "ctex"            ,
+  "xCJK2uni"        ,
+  "xeCJK"           ,
+  "xpinyin"         ,
+  "zhmetrics"       ,
+  "zhmetrics-uptex" ,
+  "zhnumber"        ,
+  "zhspacing"       ,
+}
+
+local ctan = #arg > 0 and arg or ctex_kit
+ 
+for _, pkg in ipairs(ctan) do
+  local currdir = lfs.currentdir()
+	lfs.chdir(pkg)
+	os.execute("texlua build.lua ctan")
+	lfs.chdir(currdir)
+end
