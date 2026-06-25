81a4d11b1a255b66f1dd3ca057d16d61f795caf7
Mingyu Hsia <myhsia@outlook.com>
2026-06-25 21:38:49 +0800

    doc(zhlineskip):Finished Version History

--- zhlineskip/build.lua
+++ zhlineskip/build.lua
@@ -1,10 +1,25 @@
-module       = "zhlineskip"
+module              = "zhlineskip"
 
-unpackfiles  = {module .. ".dtx"}
-installfiles = {module .. ".sty", module .. ".ins"}
+sourcefiles         = {module .. ".dtx", "*.pdf"}
+installfiles        = {module .. ".sty", module .. ".ins"}
+demofiles           = {module .. "-*-test.tex"}
+unpackfiles         = {module .. ".dtx"}
 
-stdengine    = "pdftex"
-checkengines = {"pdftex"}
-checkruns    = 1
+stdengine           = "pdftex"
+checkengines        = {"pdftex"}
+checkruns           = 1
+
+function unpack_posthook()
+  if install_files_bool then
+    for _,i in ipairs{"ctxdoc.cls", "ctxdocstrip.tex", "ctex-zhconv*.lua"} do
+      cp(i, supportdir, localdir)
+    end
+  end
+end
+function docinit_hook()
+  -- run(typesetdir, "curl -O -L " ..)
+  cp(ctanreadme, unpackdir, currentdir)
+  return 0
+end
 
 dofile("../support/build-config.lua")
